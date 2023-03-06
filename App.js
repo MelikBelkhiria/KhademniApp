d4b38ad687ee5d40bbbf3f9f8559eff862321976
MelikBelkhiria <melik.belkhiria@medtech.tn>
2023-03-06 12:51:04 +0100

    App.

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <NavigationContainer  >
       <Stack.Navigator initialRouteName="ServicePosterHome">
-        <Stack.Screen options={{headerShown:false}} name="Home" component={ServicePosterHome} />
+        <Stack.Screen options={{headerShown:true}} name="Home" component={ServicePosterHome} />
         <Stack.Screen name="Confirmation" component={Confirmation} />
       </Stack.Navigator>
     </NavigationContainer>
