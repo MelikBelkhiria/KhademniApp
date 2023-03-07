83e6b20d61e4a66262a148002907b89b78814acb
MelikBelkhiria <melik.belkhiria@medtech.tn>
2023-03-07 21:07:12 +0100

    V3

--- App.js
+++ App.js
@@ -8,7 +8,10 @@ import Navigation from "./Navigation/Navigation"
 import Login from './Components/Ahmed/LoginandHomeScreeen/login';
 import Home from "./Components/Ahmed/LoginandHomeScreeen/homescreen"
 import ProfileposterForseeker from './Components/Ahmed/ProfileposterForseeker';
-import Profileofseekerforseeker from './Components/Ahmed/profileofseekerforseeker'
+import Profileofseekerforseeker from './Components/Ahmed/profileofseekerforseeker';
+import ServicePoster3 from './Components/Yosr/ServicePoster3';
+import ServicePoster5 from './Components/Yosr/ServicePoster5';
+import ServiceSeeker3 from './Components/Yosr/ServiceSeeker3';
 
 
 const Stack = createStackNavigator();
@@ -16,7 +19,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer  >
-      <Stack.Navigator initialRouteName="profileposterForseeker">
+      <Stack.Navigator initialRouteName="ServicePoster5">
         <Stack.Screen options={{headerShown:false}} name="ServicePoster" component={ServicePosterHome} />
         <Stack.Screen name="Confirmation" component={Confirmation} />
         <Stack.Screen name="RegistrationScreen" component={RegistrationScreen}/>
@@ -24,6 +27,10 @@ export default function App() {
         <Stack.Screen name="profileofseekerforseeker" component={Profileofseekerforseeker}/>
         <Stack.Screen name="profileposterForseeker" component={ProfileposterForseeker}/>
         <Stack.Screen name="login" component={Login}/>
+        <Stack.Screen name="ServicePoster3" component={ServicePoster3}/>
+        <Stack.Screen name="ServicePoster5" component={ServicePoster5}/>
+        <Stack.Screen name="ServiceSeeker3" component={ServiceSeeker3}/>
+
 
 
       </Stack.Navigator>
