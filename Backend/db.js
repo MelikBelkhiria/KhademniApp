ff90acd0ca6854a2fa1c4b6c3db451560d44e360
Skander999 <skander.ghariani@medtech.tn>
2023-03-29 18:14:20 +0100

    registration

---
+++ Backend/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'scanpower99',
+  database: 'khademnifinal',
+  waitForConnections: true,
+});
+
+module.exports = pool;
--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const exampleRoute = require('./routes/exampleRoute');
-
+const authRoutes = require('./routes/authRoutes'); 
 const app = express();
 const port = process.env.PORT || 3001;
 
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 // Routes
 app.use('/example', exampleRoute);
-
+app.use('/api/auth', authRoutes);
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
